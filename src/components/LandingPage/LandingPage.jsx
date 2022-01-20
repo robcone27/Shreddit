1621813c9c67cd68f10e3108d7016ca07ec8c1cc
Rob Cone <robcone86@gmail.com>
2022-01-20 05:21:41 -0600

    updated landing page

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -14,29 +14,23 @@ function LandingPage() {
   };
 
   return (
-    
-    <div className="container">
-       {/* <div className="execute"> */}
-      <h2>{heading}</h2>
 
+    <div className="container">
+      <h2>{heading}</h2>
       <div className="grid">
         <div className="grid-col grid-col_8">
-          
         </div>
         <div className="grid-col grid-col_4">
-         
-          <RegisterForm />
-          </div>
-          <center>
-            <h4>Already a Member?</h4>
-            <button className="btn btn_sizeSm" onClick={onLogin}>
-              Login
-            </button>
-          </center>
-          
         </div>
-      
-    {/* </div> */}
+
+        <div className="execute">
+          <RegisterForm />
+          <h4>Already a Member?</h4>
+          <button className="btn btn_sizeSm" onClick={onLogin}>
+            Login
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
